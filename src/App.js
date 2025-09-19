4b0f2c650ee543581622978fd1fdfca40a8394d6
SomanaM <somanamaqsood03@gmail.com>
2025-09-19 16:23:12 +0500

    Add and Delete functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './MYComponents/Header';
+import Todos from './MYComponents/Todos';
+import Footer from './MYComponents/footer';
+import React, { useState, useEffect } from 'react';
+import AddTodo from './MYComponents/AddTodo';
 
 function App() {
+  let initTodo;
+
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+
+   useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const onDelete = (todo) => {
+    console.log("I am onDelete of todo", todo);
+    setTodos(todos.filter((e) => e !== todo));
+  }
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo", title, desc);
+    const myTodo = {
+      sno: todos.length + 1,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="My Todos List" searchBar={false} />
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/MYComponents/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ export const footer = () => {
+  
+  return (
+    <footer className='bg-dark text-light text-center py-3'>
+    Copyright &copy;MyTodosList.com    
+    </footer>
+  )
+}
+
+export default footer
